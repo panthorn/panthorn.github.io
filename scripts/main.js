6513207b214a69a92454f3f313318ea5814beb85
panthorn <36488234+panthorn@users.noreply.github.com>
2018-07-23 20:43:02 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,13 @@ if(!localStorage.getItem('name')) {
 myButton.onclick = function() {
   setUserName();
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
