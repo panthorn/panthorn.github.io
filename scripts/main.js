99cc4065f32f33c984d0baa3228ff97238e7005c
panthorn <36488234+panthorn@users.noreply.github.com>
2018-07-25 12:07:35 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,3 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === "images/kredittkort.jpg") {
-      myImage.setAttribute ('src',"images/mastercard.jpg");
-    } else {
-      myImage.setAttribute ('src',"images/kredittkort.jpg");
-    }
-}
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
